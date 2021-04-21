95b6038a14ef1499f27c0ca374911b8a9944241b
Mathias Fuchs <mathias_fuchs_87@yahoo.de>
2021-04-21 10:42:51 +0200

    basic 1 finished

---
+++ Mathias/basic1.php
@@ -0,0 +1,34 @@
+<?php
+
+
+$name = "Mathias";
+
+// ------------FOR LOOP--------------
+
+
+
+for ($i = 0; $i < 50; $i++) {
+
+    echo "$i iteration for $name <br>";
+
+}
+
+
+// --------------WHILE LOOP-------------
+
+
+// $i = 0;
+// while($i < 50) {
+//     $i ++;
+//     echo "$i iteration for $name <br>";
+// }
+
+
+// -------------------DO WHILE LOOP-------------------
+
+
+// $i = 0;
+// do {
+//     echo "$i iteration for $name <br>";
+//     $i++;
+// } while ($i < 50);
